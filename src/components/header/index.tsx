e402bfd1b29a6da39b39d5c890f6e21cd76fcea3
Boffin-ux <fnamily@mail.ru>
2022-11-11 16:06:46 +0300

    feat: add sticky header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './index.module.scss';
 import SelectionLang from 'components/selectionLang';
 import { useTranslation } from 'react-i18next';
@@ -7,9 +7,27 @@ import { VIEWPATH } from 'utils/variables';
 
 export default function Header() {
   const { t } = useTranslation();
+  const header = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    if (header.current) {
+      const { current } = header;
+
+      const handleScroll = () => {
+        window.pageYOffset > current.offsetHeight
+          ? current.classList.add(`${styles.scrolled}`)
+          : current.classList.remove(`${styles.scrolled}`);
+      };
+      window.addEventListener('scroll', handleScroll);
+
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }
+  }, []);
 
   return (
-    <header className="header">
+    <header className={styles.header} ref={header}>
       <div className={styles.wrapper}>
         <NavLink className={styles.logo} to={VIEWPATH.MAIN} end>
           PM-APP

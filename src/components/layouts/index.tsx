243667359091dc5ebd0540593be73acd793cc8a1
Boffin-ux <fnamily@mail.ru>
2022-11-15 20:49:51 +0300

    refactor: remove custome stiles

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import Footer from 'components/footer/Footer';
 import Header from 'components/header/Header';
 import React from 'react';
@@ -7,9 +8,16 @@ const Layout = () => {
   return (
     <>
       <Header />
-      <main className="main">
+      <Box
+        sx={{
+          display: 'flex',
+          flexGrow: '1',
+          background: 'linear-gradient(45deg,rgba(63, 191, 195, 0.8),#333)',
+        }}
+        component="main"
+      >
         <Outlet />
-      </main>
+      </Box>
       <Footer />
     </>
   );

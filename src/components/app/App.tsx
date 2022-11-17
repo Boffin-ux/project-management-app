0a7921438302eb0fc12ce66720b5df365fcf6008
Boffin-ux <fnamily@mail.ru>
2022-11-17 08:22:43 +0300

    refactor: add CssBaseline remove Normalize

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -10,21 +10,25 @@ import HomePage from 'pages/homePage/HomePage';
 import { Boards } from 'pages/boardList/BoardsList';
 import { muiTheme } from 'utils/muiTheme';
 import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import './app.scss';
 
 export default function App() {
   const store = setupStore();
   return (
     <Provider store={store}>
       <ThemeProvider theme={muiTheme}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<HomePage />} />
-              <Route path={VIEW_PATH.BOARDS} element={<Boards />} />
-              <Route path={VIEW_PATH.REST} element={<PageNotFound />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <CssBaseline>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<HomePage />} />
+                <Route path={VIEW_PATH.BOARDS} element={<Boards />} />
+                <Route path={VIEW_PATH.REST} element={<PageNotFound />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </CssBaseline>
       </ThemeProvider>
     </Provider>
   );

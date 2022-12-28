f6803947ee9a35f9eceeefc8bf58e67c08e951ed
Boffin-ux <fnamily@mail.ru>
2022-12-28 11:26:43 +0300

    feat: add suspense loading components

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,16 +1,10 @@
+import React, { useEffect, lazy, Suspense } from 'react';
 import AuthRedirect from 'components/AuthRedirect/AuthRedirect';
-import Layout from 'components/layout/Layout';
 import { useAppDispatch, useAppSelector } from 'hooks/redux';
 import { SnackbarProvider } from 'notistack';
-import Board from 'pages/boardItem/BoardItem';
-import Boards from 'pages/boardList/BoardsList';
 import ErrorBoundaryPage from 'pages/errorBoundaryPage/errorBoundaryPage';
 import HomePage from 'pages/homePage/HomePage';
-import PageNotFound from 'pages/page404/Page404';
-import Profile from 'pages/profile/Profile';
-import SignIn from 'pages/signIn/SignIn';
-import SignUp from 'pages/signUp/SignUp';
-import React, { useEffect } from 'react';
+import Layout from 'components/layout/Layout';
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -22,6 +16,14 @@ import { getUserInfo } from 'store/user/thunks';
 import { VIEW_PATH } from 'utils/variables';
 import '../../i18n/i18next';
 import './app.scss';
+import Loader from 'components/universal/Loader/Loader';
+
+const PageNotFound = lazy(() => import('pages/page404/Page404'));
+const Profile = lazy(() => import('pages/profile/Profile'));
+const Boards = lazy(() => import('pages/boardList/BoardsList'));
+const Board = lazy(() => import('pages/boardItem/BoardItem'));
+const SignIn = lazy(() => import('pages/signIn/SignIn'));
+const SignUp = lazy(() => import('pages/signUp/SignUp'));
 
 export default function App() {
   const dispatch = useAppDispatch();
@@ -38,18 +40,60 @@ export default function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-        <Route path="/" element={<Layout />}>
+        <Route path={VIEW_PATH.HOME} element={<Layout />}>
           <Route errorElement={<ErrorBoundaryPage />}>
             <Route index element={<HomePage />} />
-            <Route path={VIEW_PATH.REST} element={<PageNotFound />} />
+            <Route
+              path={VIEW_PATH.REST}
+              element={
+                <Suspense fallback={<Loader size={110} />}>
+                  <PageNotFound />
+                </Suspense>
+              }
+            />
             <Route element={<AuthRedirect withAuth={false} />}>
-              <Route path={VIEW_PATH.SIGN_UP} element={<SignUp />} />
-              <Route path={VIEW_PATH.SIGN_IN} element={<SignIn />} />
+              <Route
+                path={VIEW_PATH.SIGN_UP}
+                element={
+                  <Suspense fallback={<Loader size={110} />}>
+                    <SignUp />
+                  </Suspense>
+                }
+              />
+              <Route
+                path={VIEW_PATH.SIGN_IN}
+                element={
+                  <Suspense fallback={<Loader size={110} />}>
+                    <SignIn />
+                  </Suspense>
+                }
+              />
             </Route>
             <Route element={<AuthRedirect withAuth />}>
-              <Route path={VIEW_PATH.BOARDS} element={<Boards />} />
-              <Route path={VIEW_PATH.BOARD} element={<Board />} />
-              <Route path={VIEW_PATH.PROFILE} element={<Profile />} />
+              <Route
+                path={VIEW_PATH.BOARDS}
+                element={
+                  <Suspense fallback={<Loader size={110} />}>
+                    <Boards />
+                  </Suspense>
+                }
+              />
+              <Route
+                path={VIEW_PATH.BOARD}
+                element={
+                  <Suspense fallback={<Loader size={110} />}>
+                    <Board />
+                  </Suspense>
+                }
+              />
+              <Route
+                path={VIEW_PATH.PROFILE}
+                element={
+                  <Suspense fallback={<Loader size={110} />}>
+                    <Profile />
+                  </Suspense>
+                }
+              />
             </Route>
           </Route>
         </Route>
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,8 +1,6 @@
 import { Box } from '@mui/material';
 import Footer from 'components/footer/Footer';
 import Header from 'components/header/Header';
-import Loader from 'components/universal/Loader/Loader';
-import { useAppSelector } from 'hooks/redux';
 import { useDocumentTitle } from 'hooks/useDocumentTitle';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
@@ -10,11 +8,6 @@ import styles from './Layout.module.scss';
 
 const Layout = () => {
   useDocumentTitle();
-  const { isLoading } = useAppSelector((state) => state.user);
-
-  // if (isLoading) {
-  //   return <Loader size={110} />;
-  // }
 
   return (
     <>
